3988c1e9cf0fa155ea5293f823e269c4ff372d20
pedrolago-web <pedrolago2001@gmail.com>
2025-11-17 20:20:21 +0000

    alteração

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -15,5 +15,37 @@ int main() {
     inicializarTabuleiro(tabuleiro);
 
     printf("Posicionando navios...\n");
+
+if (posicionarNavioHorizontal(tabuleiro, 2, 3)) {
+        printf("Navio horizontal posicionado com sucesso!\n");
+    } else {
+        printf("Erro: Posição inválida para navio horizontal!\n");
+        return 1;
+    }
+
+if (posicionarNavioVertical(tabuleiro, 5, 7)) {
+        printf("Navio vertical posicionado com sucesso!\n");
+    } else {
+        printf("Erro: Posição inválida para navio vertical!\n");
+        return 1;
+    }
+
+ printf("\nTabuleiro do Batalha Naval:\n");
+    exibirTabuleiro(tabuleiro);
+
     return 0;
 }
+
+void inicializarTabuleiro(int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO]) {
+    for (int i = 0; i < TAM_TABULEIRO; i++) {
+        for (int j = 0; j < TAM_TABULEIRO; j++) {
+            tabuleiro[i][j] = 0; 
+        }
+    }
+}
+
+int posicionarNavioHorizontal(int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO], int linha, int coluna) {
+    // Verifica se o navio cabe horizontalmente a partir da coluna inicial
+    if (coluna + TAM_NAVIO > TAM_TABULEIRO) {
+        return 0; // Navio ultrapassa o limite direito do tabuleiro
+    }
