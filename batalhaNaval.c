f8f828c2d9a59636dd9de7845a6e5d2146e0d092
pedrolago-web <pedrolago2001@gmail.com>
2025-11-17 20:16:21 +0000

    constante

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+#define TAM_TABULEIRO 10
+#define TAM_NAVIO 3
+
+void inicializarTabuleiro(int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO]);
+int posicionarNavioHorizontal(int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO], int linha, int coluna);
+int posicionarNavioVertical(int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO], int linha, int coluna);
+void exibirTabuleiro(int tabuleiro[TAM_TABULEIRO][TAM_TABULEIRO]);
+
 int main() {
 
 
